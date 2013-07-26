adb86bda38d0daba603cd934f1dc2ce644497422
SheldonJackson <sjackson@veteransunited.com>
2013-07-26 11:06:57 -0500

    Testing Merging

--- Items/Item.cs
+++ Items/Item.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//Testing out Merging
 namespace VuMud.Items {
     public abstract class Item : IEquipable, IConsumable
     {
