77f323ccc1b7c395392f41b88a6d3018378b42ee
mam8cc <mam8cc@gmail.com>
2013-07-29 10:16:57 -0500

    Added MoveController and Menu

---
+++ Controllers/RoomController.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using VuMud.Menus;
+using VuMud.World;
+
+namespace VuMud.Controllers {
+    class RoomController : IController {
+        public Creature.Creature PlayerCreature { get; set; }
+        public RoomMenu RoomMenu { get; set; }
+        public Map Map { get; set; }
+
+        public RoomController (Creature.Creature character, Map world)
+        {
+            RoomMenu = new RoomMenu();
+            PlayerCreature = character;
+            Map = Map;
+        }
+
+        public void DisplayMenu()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void HandleResponse()
+        {
+            Room room = PlayerCreature.Location;
+
+            var response = RoomMenu.GetRepsonse();
+            switch (response.ToUpper())
+            {
+                case "L":
+                    RoomMenu.Display(room);
+
+                    break;
+                case "E":
+                    var menuItems = (from exit in room.Exits where exit != null select new MenuItem(exit, exit.Substring(0, 1))).ToList();
+                    RoomMenu.Display(menuItems);
+
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,26 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using VuMud.Controllers;
 using VuMud.Creature;
-using VuMud.Menus;
 using VuMud.World;
 
 namespace VuMud {
     class Program {
         static void Main(string[] args) {
             Map worldMap = new Map();
-            PlayerCharacter pc = new PlayerCharacter();
-            pc.Location = worldMap.WorldMapRooms[0, 0];
-            MoveController mc = new MoveController(pc, worldMap);
+            PlayerCharacter playerCharacter = new PlayerCharacter();
+            playerCharacter.Location = worldMap.WorldMapRooms[0, 0];
+            MoveController moveController = new MoveController(playerCharacter, worldMap);
             worldMap.DisplayMap();
             do
             {
                 try {
-                    mc.DisplayMenu();
-                    mc.HandleResponse();
+
+                    moveController.DisplayMenu();
+                    moveController.HandleResponse();
                 }
                 catch (Exception e) {
                     Console.WriteLine(e.Message);
