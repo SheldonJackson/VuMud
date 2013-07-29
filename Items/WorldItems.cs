77851b3b4abf8e67ae0aeb01152a3b2beebe592a
SheldonJackson <sjackson@veteransunited.com>
2013-07-29 10:50:25 -0500

    Updating Item Creation

--- Items/WorldItems.cs
+++ Items/WorldItems.cs
@@ -14,19 +14,19 @@ namespace VuMud.Items {
         {
             Items.Add(new Potion(10, Stats.Health, "Recovers 10 Health", "Minor Health Potion"));
             Items.Add(new Potion(10, Stats.Mana, "Recovers 10 Mana", "Minor Mana Potion"));
-            Items.Add(new Weapon(2, Stats.AttackBonus, "Basic Starting Sword.", "Iron Sword"));
-            Items.Add(new Weapon(1, Stats.AttackBonus, "Basic Starting Dagger.", "Iron Dagger"));
-            Items.Add(new Weapon(2, Stats.AttackBonus, "Basic Stargin Axe.", "Iron Axe"));
-            Items.Add(new Armor(1, Stats.Armor, "It's a hat.", "Leather Cap"));
-            Items.Add(new Armor(2, Stats.Charisma, "Not that shiny, but looks nice.", "Bronze Charm"));
-            Items.Add(new Armor(1, Stats.Armor, "Welcome to the 80's!", "Leather Shoulder Pads"));
-            Items.Add(new Armor(1, Stats.Armor, "Basic Gauntlets of the Poor", "Leather Gauntlets"));
-            Items.Add(new Armor(4, Stats.Armor, "Provides Basic coverage for your squishy bits.", "Leather Tunic"));
-            Items.Add(new Armor(1, Stats.Armor, "Don't be saggin', bro.", "Leather Belt"));
-            Items.Add(new Armor(2, Stats.Armor, "Better than Chaps!", "Leather Pants"));
-            Items.Add(new Armor(2, Stats.Armor, "Keep yo feet clean!", "Leather Boots"));
-            Items.Add(new Armor(2, Stats.Constitution, "Bling!", "Iron Ring"));
-            Items.Add(new Armor(1, Stats.Intelligence, "Bling!", "Opal Ring"));
+            Items.Add(new Weapon(2, Stats.AttackBonus, "Basic Starting Sword.", "Iron Sword", 5));
+            Items.Add(new Weapon(1, Stats.AttackBonus, "Basic Starting Dagger.", "Iron Dagger", 1));
+            Items.Add(new Weapon(2, Stats.AttackBonus, "Basic Stargin Axe.", "Iron Axe", 4));
+            Items.Add(new Armor(1, Stats.Armor, "It's a hat.", "Leather Cap", 1));
+            Items.Add(new Armor(2, Stats.Charisma, "Not that shiny, but looks nice.", "Bronze Charm", 1));
+            Items.Add(new Armor(1, Stats.Armor, "Welcome to the 80's!", "Leather Shoulder Pads", 2));
+            Items.Add(new Armor(1, Stats.Armor, "Basic Gauntlets of the Poor", "Leather Gauntlets", 1));
+            Items.Add(new Armor(4, Stats.Armor, "Provides Basic coverage for your squishy bits.", "Leather Tunic", 5));
+            Items.Add(new Armor(1, Stats.Armor, "Don't be saggin', bro.", "Leather Belt", 1));
+            Items.Add(new Armor(2, Stats.Armor, "Better than Chaps!", "Leather Pants", 3));
+            Items.Add(new Armor(2, Stats.Armor, "Keep yo feet clean!", "Leather Boots", 2));
+            Items.Add(new Armor(2, Stats.Constitution, "Bling!", "Iron Ring", 1));
+            Items.Add(new Armor(1, Stats.Intelligence, "Bling!", "Opal Ring", 1));
         }
 
         public Item GenerateRandomWorldItem()
