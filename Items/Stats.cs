baf844c4ad914f6c9aa2462fe6e27b4808aec62c
SheldonJackson <sjackson@veteransunited.com>
2013-07-29 10:45:49 -0500

    New Item Functionality

---
+++ Items/Stats.cs
@@ -0,0 +1,16 @@
+﻿namespace VuMud.Items {
+    public enum Stats {
+        Strength,
+        Dexterity,
+        Constitution,
+        Intelligence,
+        Wisdom,
+        Charisma,
+        Health,
+        Mana,
+        Armor,
+        Damage,
+        AttackBonus,
+        None
+    }
+}
