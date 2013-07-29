1e39f2ab0e81b51e3c35a2eddb528b02ce0afee2
SheldonJackson <sjackson@veteransunited.com>
2013-07-29 16:16:20 -0500

    Fixing Program.cs

--- Program.cs
+++ Program.cs
@@ -13,26 +13,34 @@ namespace VuMud {
     class Program {
         static void Main(string[] args)
         {
+            PlayerCharacter pc = new PlayerCharacter();
+            var characterCreationController = new CharacterCreationController();
+            pc = characterCreationController.CreateCharacter();
+            // TODO - This will need to be rewritten or taken out, right now it's just here to prove that character creation works
+            Console.WriteLine("Here is your character's info!");
+            Console.WriteLine(pc.Name);
+            Console.WriteLine(pc.Description);
+            Console.WriteLine();
 
-            SqlExecute dbm = new SqlExecute();
-            dbm.SetConfiguration("VUHL-9CLYBS1\\CSHARPCLASS", "VehicleInventory", "Vehicles", "Password123!");
+            Console.WriteLine("Get ready for an adventure!!");
+            Console.WriteLine();
+            Map worldMap = new Map();
+            MoveController mc = new MoveController(pc, worldMap);
+            pc.Location = worldMap.WorldMapRooms[0, 0];
+            PlayerCharacter playerCharacter = new PlayerCharacter();
+            playerCharacter.Location = worldMap.WorldMapRooms[0, 0];
+            MoveController moveController = new MoveController(playerCharacter, worldMap);
+            worldMap.DisplayMap();
 
-            var parameters = new SqlParameter[]
-            {
-                    new SqlParameter("name", "DBM TESTER"),
-                    new SqlParameter("type", 2),
-                    new SqlParameter("color","Yellow")
-            };
+            do {
+                try {
 
-            List<IDataRecord> resultSet = dbm.Select("SelectVehicles", parameters);
-            foreach (IDataRecord result in resultSet)
-            {
-                Console.WriteLine(result["Name"]);
-            }
-
-            
-            Console.ReadLine();
-
+                    moveController.DisplayMenu();
+                    moveController.HandleResponse();
+                } catch (Exception e) {
+                    Console.WriteLine(e.Message);
+                }
+            } while (true);
 
         }
     }
