340cce37dfd330318c01848e1cbbd2b03d1cc158
SheldonJackson <sjackson@veteransunited.com>
2013-07-29 16:11:05 -0500

    DBM Integration

---
+++ Dbm/DbConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VuMud.Dbm {
+    public class DbConfiguration {
+        private string Server { get; set; }
+        private string Database { get; set; }
+        private string User { get; set; }
+        private string Password { get; set; }
+
+        public DbConfiguration(string server, string database, string user, string password)
+        {
+            Server = server;
+            Database = database;
+            User = user;
+            Password = password;
+        }
+
+        public string GetDbConfigurationString()
+        {
+            return String.Format("SERVER={0};Database={1};User Id={2};Password={3}",
+                Server,
+                Database,
+                User,
+                Password
+                );
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,42 +1,39 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Xml.Linq;
 using VuMud.Controllers;
 using VuMud.Creature;
+using VuMud.Dbm;
 using VuMud.World;
 
 namespace VuMud {
     class Program {
-        static void Main(string[] args) {
-            
-            PlayerCharacter pc = new PlayerCharacter();
-            var characterCreationController = new CharacterCreationController();
-            pc = characterCreationController.CreateCharacter();
-            // TODO - This will need to be rewritten or taken out, right now it's just here to prove that character creation works
-            Console.WriteLine("Here is your character's info!");
-            Console.WriteLine(pc.Name);
-            Console.WriteLine(pc.Description);
-            Console.WriteLine();
+        static void Main(string[] args)
+        {
 
-            Console.WriteLine("Get ready for an adventure!!");
-            Console.WriteLine();
-            Map worldMap = new Map();
-            MoveController mc = new MoveController(pc, worldMap);
-            pc.Location = worldMap.WorldMapRooms[0, 0];
-            PlayerCharacter playerCharacter = new PlayerCharacter();
-            playerCharacter.Location = worldMap.WorldMapRooms[0, 0];
-            MoveController moveController = new MoveController(playerCharacter, worldMap);
-            worldMap.DisplayMap();
+            SqlExecute dbm = new SqlExecute();
+            dbm.SetConfiguration("VUHL-9CLYBS1\\CSHARPCLASS", "VehicleInventory", "Vehicles", "Password123!");
 
-            do
+            var parameters = new SqlParameter[]
             {
-                try {
+                    new SqlParameter("name", "DBM TESTER"),
+                    new SqlParameter("type", 2),
+                    new SqlParameter("color","Yellow")
+            };
 
-                    moveController.DisplayMenu();
-                    moveController.HandleResponse();
-                }
-                catch (Exception e) {
-                    Console.WriteLine(e.Message);
-                }
-            } while (true);
+            List<IDataRecord> resultSet = dbm.Select("SelectVehicles", parameters);
+            foreach (IDataRecord result in resultSet)
+            {
+                Console.WriteLine(result["Name"]);
+            }
+
+            
+            Console.ReadLine();
+
+
         }
     }
 }
