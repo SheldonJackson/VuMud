a053c65362dab311ee7391e720854a5e5ed47cb1
alewis19 <alewis@veteransunited.com>
2013-07-30 08:31:57 -0500

    ItemDao skeleton

---
+++ Items/DataAccess/ItemDao.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace VuMud.Items.DataAccess {
+    public class ItemDao {
+        public SqlConnection Connection;
+
+        public ItemDao() {
+            Connection = new SqlConnection("Server=localhost;Database=VuMud;User Id=vehical_admin;Password=tester;");
+        }
+
+        public SqlDataReader RetrieveItem(int itemId) {
+            using (Connection) {
+
+            }
+        }
+    }
+}
