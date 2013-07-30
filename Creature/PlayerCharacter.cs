9c443133fc58a991d1faa38347106d77d168346e
erahm <darkstar1984@gmail.com>
2013-07-30 12:45:28 -0500

    format character info updates

--- Creature/PlayerCharacter.cs
+++ Creature/PlayerCharacter.cs
@@ -1,4 +1,5 @@
-﻿using VuMud.Items;
+﻿using System;
+using VuMud.Items;
 
 namespace VuMud.Creature {
     public class PlayerCharacter : Creature {
@@ -9,6 +10,29 @@ namespace VuMud.Creature {
 
         public string Inspect(Creature creature) {
             return creature.Description;
+        }
+
+        public string FormatCharacterInfo() {
+            string formattedString = "";
+
+            return formattedString;
+        }
+
+        private string _formatStats() {
+            return string.Format(
+                "Combat Stats: {0}" +
+                "    Health: {1}{0}" +
+                "    Mana: {2}{0}" +
+                "    Armor: {3}{0}" +
+                "    Attack Bonus: {4}{0}" +
+                "    Damage: {5}{0}{0}",
+                Environment.NewLine,
+                Stats.Health,
+                Stats.Mana,
+                Stats.Armor,
+                Stats.AttackBonus,
+                Stats.Damage
+            );
         }
     }
 }
